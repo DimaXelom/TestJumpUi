4b52ac39c26680962cf703444c3176956a844f65
DimaBrisset <DimaBrisset@gmail.com>
2023-09-04 17:25:53 +0300

    04.09.2023
    04.09.2023

--- Assets/Scripts/RotationSettings.cs
+++ Assets/Scripts/RotationSettings.cs
@@ -9,28 +9,28 @@ public interface ICopyable<T>
 
 public class RotationSettings : MonoBehaviour, ICopyable<RotationSettings>
 {
-    [SerializeField] private float minPitchAngle = -80f;
-    [SerializeField] private float maxPitchAngle = 80f;
-    [SerializeField] private RotationBehavior rotationBehavior = RotationBehavior.Continuous;
+    [SerializeField] private float _minPitchAngle = -80f;
+    [SerializeField] private float _maxPitchAngle = 80f;
+    [SerializeField] private RotationBehavior _rotationBehavior = RotationBehavior.Continuous;
 
-    [SerializeField] private float minRotationSpeed = 20f;
-    [SerializeField] private float maxRotationSpeed = 360f;
+    [SerializeField] private float _minRotationSpeed = 20f;
+    [SerializeField] private float _maxRotationSpeed = 360f;
 
-    public float MinPitchAngle => minPitchAngle;
-    public float MaxPitchAngle => maxPitchAngle;
-    public RotationBehavior RotationBehavior => rotationBehavior;
-    public float MinRotationSpeed => minRotationSpeed;
-    public float MaxRotationSpeed => maxRotationSpeed;
+    public float MinPitchAngle => _minPitchAngle;
+    public float MaxPitchAngle => _maxPitchAngle;
+    public RotationBehavior RotationBehavior => _rotationBehavior;
+    public float MinRotationSpeed => _minRotationSpeed;
+    public float MaxRotationSpeed => _maxRotationSpeed;
 
     public RotationSettings Copy()
     {
         return new RotationSettings()
         {
-            minPitchAngle = minPitchAngle,
-            maxPitchAngle = maxPitchAngle,
-            rotationBehavior = rotationBehavior,
-            minRotationSpeed = minRotationSpeed,
-            maxRotationSpeed = maxRotationSpeed
+            _minPitchAngle = _minPitchAngle,
+            _maxPitchAngle = _maxPitchAngle,
+            _rotationBehavior = _rotationBehavior,
+            _minRotationSpeed = _minRotationSpeed,
+            _maxRotationSpeed = _maxRotationSpeed
         };
     }
 }
